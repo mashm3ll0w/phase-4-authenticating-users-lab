58f424052f1c34fcc8c11b4878ac56778570f43b
Charles Swaleh <charlswall@live.com>
2023-05-01 15:58:15 +0300

    Log a user in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show]
+  resources :articles, only: %i[index show]
+  post "/login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
